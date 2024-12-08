0af97a6709158c67a0fc921798d9ae16144533fc
Refkell <gfhx393@gmail.com>
2024-12-08 17:42:32 +0300

    Quartz sync: Dec 8, 2024, 5:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "☄️ Shattered Vessel",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
